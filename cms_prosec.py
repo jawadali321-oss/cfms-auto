34efdb17add5abffc5a10611a07e0b99024d3193
jawadali321-oss <jawadali321@example.com>
2026-03-03 22:26:46 +0500

    fix indentation after removing remote-debugging flag

--- cms_prosec.py
+++ cms_prosec.py
@@ -233,7 +233,7 @@ class BrowserManager:
                 opts.add_argument("--disable-software-rasterizer")
                 opts.add_argument("--disable-extensions")
                 opts.add_argument("--disable-dev-tools")
-                                opts.add_experimental_option('excludeSwitches', ['enable-logging'])
+                                                opts.add_experimental_option('excludeSwitches', ['enable-logging'])
                 opts.add_experimental_option("detach", False)
                 
                 logging.info("🚀 Starting HEADLESS browser - completely invisible!")
